c11cb13dfaaa0a170d1d380df3bdd3ec67677d93
berkaysengel <berkaysengel@hotmail.com>
2023-06-05 03:32:44 +0300

    Project-2

---
+++ AzureVision/Data/FileUpload.cs
@@ -0,0 +1,26 @@
+﻿using AzureVision.Services;
+using BlazorInputFile;
+namespace AzureVision.Data
+{
+    public class FileUpload : IFileUpload
+    {
+        private readonly IWebHostEnvironment _webHostEnvironment;
+
+
+        public FileUpload(IWebHostEnvironment webHost)
+        {
+            _webHostEnvironment = webHost;
+        }
+
+        public async Task UploadFile(IFileListEntry file)
+        {
+            var path = Path.Combine(_webHostEnvironment.ContentRootPath, "Images", file.Name);
+            var MemStream = new MemoryStream();
+            await file.Data.CopyToAsync(MemStream);
+            using (FileStream fs = new FileStream(path, FileMode.Create, FileAccess.Write))
+            {
+                MemStream.WriteTo(fs);
+            }
+        }
+    }
+}
---
+++ AzureVision/Services/IFileUpload.cs
@@ -0,0 +1,8 @@
+﻿using BlazorInputFile;
+namespace AzureVision.Services
+{
+    public interface IFileUpload
+    {
+        Task UploadFile(IFileListEntry file);
+    }
+}
